e9d7caa2825a0fa22eb4a6f78a582578b7e8d7ed
canaydogan <canaydogan0568@gmail.com>
2024-12-18 13:44:43 +0300

    Update in the upside down path

--- drawFunc.c
+++ drawFunc.c
@@ -27,7 +27,7 @@ void draw (int n) {
 void drawUpsideDown (int n) {
     int i, j;
     for (i = n; i > 1; i--) {
-        for (j = 0; j < i; j++) {
+        for (j = 0; j < i - 1; j++) {
             printf("#");
         }
         printf("\n");
